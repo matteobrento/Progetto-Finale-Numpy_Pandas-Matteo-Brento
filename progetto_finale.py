adcd96d2024b4eeb0ed26b850ea0b0cd47236cbe
matteobrento <matteobrento.workaddress@gmail.com>
2024-07-18 15:39:39 +0200

    Add files via upload
    Aggiornamento

--- progetto_finale.py
+++ progetto_finale.py
@@ -33,13 +33,83 @@ su altri fattori.
 Valutare la performance del modello attraverso metriche come l'accuratezza e l'AUC (Area Under Curve).
 """
 
-import crea_dataset as cd
 import pandas as pd
 import numpy as np
 
 def carica_da_csv():
-
-    file_path = 'dataframe.csv'
+    file_path = 'C:\\Users\\matte\\AcademyItConsulting\\Esercitazioni\\18.07\\dataframe.csv'
     df = pd.read_csv(file_path)
     return df
+
+df = carica_da_csv()
+
+def distribuzione_dati(df):
+    descrizione = df.describe(include='all')
+    print("Distribuzione dati con describe: \n", descrizione, "\n")
+
+def informazione_dati(df):
+    info = df.info()
+    print("Info: \n", info, "\n")
+
+def valori_mancanti(df):
+    missing_values = df.isnull().sum()
+    print("Valori mancanti per colonna: \n", missing_values, "\n")
+
+def pulizia_dati(df):
+    df_cleaned = df.dropna()
+    print("Rimozione delle righe dove almeno un elemento è mancante: \n", df_cleaned, "\n")
+    return df_cleaned
+
+def verifica_anomalie(df):
+    df = df[(df['Età'] > 0) & (df['Età'] <= 100)]
+    df = df[df['Tariffa_Mensile'] > 0]
+    return df
+
+df_senza_anomalie = verifica_anomalie(df)
+print("Dataset dopo la correzione delle anomalie: \n", df_senza_anomalie, "\n")
+
+
+def aggiungi_colonna(df_senza_anomalie):
+
+    df_senza_anomalie["Costo_per_GB"] = df_senza_anomalie["Tariffa_Mensile"]//df_senza_anomalie["Dati_Consumati"]
+    print("DataFrame con colonna Costo_per_GB: \n", df_senza_anomalie, "\n")
+    return df_senza_anomalie
+
+df = aggiungi_colonna()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+""" 
+distribuzione_dati(df)
+informazione_dati(df)
+valori_mancanti(df)
+pulizia_dati(df) 
+"""
+
+
+
+
+
+
+
+
+    
 
