2a5e824d236cc465cde790e072460f5876002286
matteobrento <matteobrento.workaddress@gmail.com>
2024-07-19 09:40:43 +0200

    Add files via upload
    Aggiornamento finale

--- main_progetto_finale.py
+++ main_progetto_finale.py
@@ -8,6 +8,7 @@ def menu():
     print("2. Pulizia dei dati e verifica delle anomalie")
     print("3. Analisi Esplorativa dei Dati (EDA)")
     print("4. Preparazione dati per la modellazione")
+    print("5. Visualizzazione grafici")
 
 compagnia = pf.Compagnia_Telefonica()
 
@@ -17,6 +18,9 @@ try:
 except FileNotFoundError:
     print("Nessun DataFrame caricato. Recuperalo tramite Salvataggio: \n")
     df = cd.salva_dataframe()
+    print("DataFrame Iniziale: \n", df, "\n")
+
+compagnia.conversione_valore_categorico_in_numerico()   #converto da categorico a numerico all'inizio così da poter effettuare tutte le operazioni matematiche in tranquillità
 
 while True:
 
@@ -27,17 +31,34 @@ while True:
         compagnia.informazione_dati()
         compagnia.valori_mancanti()
     elif opzione == "2":
-        compagnia.conversione_valore_categorico_in_numerico()
         strategia = input("\nInserisci strategia scegliendo tra rimuovi, mean o median: ")
         compagnia.pulizia_dati(strategia)
         compagnia.verifica_anomalie()
+        salvataggio = input("\nVuoi salvare il df ? ")
+        if salvataggio.lower() == "si":
+            compagnia.salva_dataframe_aggiornato()
+        else:
+            print("\nHai deciso di non salvarlo!")
     elif opzione == "3":
         compagnia.verifica_anomalie()
         compagnia.aggiungi_colonna()
         compagnia.raggruppamento()
         compagnia.correlazione()
+        salvataggio = input("\nVuoi salvare il df ? ")
+        if salvataggio.lower() == "si":
+            compagnia.salva_dataframe_aggiornato()
+        else:
+            print("Hai deciso di non salvarlo!")
     elif opzione == "4": 
+        compagnia.aggiungi_colonna()
         compagnia.normalizzazione()
+        salvataggio = input("\nVuoi salvare il df ? ")
+        if salvataggio.lower() == "si":
+            compagnia.salva_dataframe_aggiornato()
+        else:
+            print("Hai deciso di non salvarlo!")
+    elif opzione == "5":
+        compagnia.grafici()
     else:
         print("Scelta non disponibile")
 
--- progetto_finale.py
+++ progetto_finale.py
@@ -35,38 +35,39 @@ Valutare la performance del modello attraverso metriche come l'accuratezza e l'A
 
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
 class Compagnia_Telefonica:
 
-    def __init__(self):
+    def __init__(self): #costruttore con self.df impostato a none perchè va ancora caricato
         self.df = None
 
-    def carica_da_csv(self):
+    def carica_da_csv(self):    #funzione per caricare il csv
         self.file_path = 'C:\\Users\\matte\\AcademyItConsulting\\Esercitazioni\\18.07\\dataframe.csv'
         self.df = pd.read_csv(self.file_path)
         return self.df
 
-    def distribuzione_dati(self):
+    def distribuzione_dati(self):   #descrizione statistica
         self.descrizione = self.df.describe(include='all')
         print("Distribuzione dati con describe: \n", self.descrizione, "\n")
 
-    def informazione_dati(self):
+    def informazione_dati(self):    #info sul dataset
         self.info = self.df.info()
         print("Info: \n", self.info, "\n")
 
-    def valori_mancanti(self):
+    def valori_mancanti(self):  #is null controlla i valori mancanti per colonna e sum ne restituisce la somma per colonna in output
         self.missing_values = self.df.isnull().sum()
         print("Somma valori mancanti per colonna: \n", self.missing_values, "\n")
 
-    def pulizia_dati(self, strategia):
+    def pulizia_dati(self, strategia):  #strategia è il tipo di operazione che si vuole effettuare per gestire i dati mancanti
         
         if strategia == 'rimuovi':
-            self.df = self.df.dropna()
+            self.df = self.df.dropna()  #dropna è la piu drastica perchè rimuove l'intera riga laddove trova un valore mancante
             print("Rimozione delle righe con valore mancante: \n", self.df, "\n")
-        elif strategia == 'mean':
+        elif strategia == 'mean':   #sostituisce i valori mancanti con la media
             self.df.fillna(self.df.mean(), inplace=True)
             print("Sostituzioni dei valori mancanti con la media dei valori in colonna: \n", self.df, "\n")
-        elif strategia == 'median':
+        elif strategia == 'median': #sostituisce i valori mancanti con la mediana
             self.df.fillna(self.df.median(), inplace=True)
             print("Sostituzioni dei valori mancanti con la mediana dei valori in colonna: \n", self.df, "\n")
         else:
@@ -75,21 +76,20 @@ class Compagnia_Telefonica:
         print("\nDataframe dopo la pulizia dei dati: \n", self.df, "\n")
         return self.df
 
-    def verifica_anomalie(self):
+    def verifica_anomalie(self):    #restituisce un database che esclude le righe che non rispettano questi parametri
         self.df = self.df[(self.df['Età'] > 0) & (self.df['Età'] <= 100)]
         self.df = self.df[self.df['Tariffa_Mensile'] > 0]
         print("DataFrame dopo la verifica delle anomalie: \n", self.df, "\n")
         return self.df
 
-
-    def aggiungi_colonna(self):
+    def aggiungi_colonna(self): #aggiunge una colonna nuova, costo gb calcolando da altre due colonne già esistenti
 
         self.df["Costo_per_GB"] = self.df["Tariffa_Mensile"]/self.df["Dati_Consumati"]
-        self.df["Costo_per_GB"] = self.df["Costo_per_GB"].fillna(self.df["Costo_per_GB"].mean())
+        #self.df["Costo_per_GB"] = self.df["Costo_per_GB"].fillna(self.df["Costo_per_GB"].mean())
         print("DataFrame con colonna Costo_per_GB: \n", self.df, "\n")
         return self.df
 
-    def raggruppamento(self):
+    def raggruppamento(self):   #raggruppamento per churn ritornando media, mediana e std delle colonne selezionate
 
         self.raggruppamento = self.df.groupby('Churn').agg({
             'Età': ['mean', 'median', 'std'],
@@ -98,18 +98,18 @@ class Compagnia_Telefonica:
         })
         print("\nRelazione tra Età, Durata_Abbonamento, Tariffa_Mensile raggruppando per Churn: \n", self.raggruppamento, "\n")
 
-    def conversione_valore_categorico_in_numerico(self):
+    def conversione_valore_categorico_in_numerico(self):    #1: si, 0: no
 
         self.df['Churn'] = self.df['Churn'].map(lambda x: 1 if x == 'si' else 0)
         print("Conversione di Churn da Categorico a Numerico: \n", self.df, "\n")
         return self.df
 
-    def correlazione(self):
+    def correlazione(self): #correlazione
 
         self.correlazione = self.df.corr()
         print("Correlazione dei dati tramite coefficiente di Pearson tra 1  e -1: \n", self.correlazione, "\n")
 
-    def normalizzazione(self):
+    def normalizzazione(self):  #normalizzazione dei valori sulle colonne selezionate
 
         self.colonne = ['Età', 'Durata_Abbonamento', 'Tariffa_Mensile', 'Dati_Consumati', 'Servizio_Clienti_Contatti', 'Churn', 'Costo_per_GB']
         
@@ -120,6 +120,39 @@ class Compagnia_Telefonica:
         
         print("DataFrame con colonne numeriche normalizzate: \n", self.df[self.colonne], "\n")
         return self.df
+    
+    def grafici(self):  #plot generati con l'utilizzo di matplotlib, i primi due sono grafici a barre che mostrano la distribuzione dei dati della colonna selezionata (Tariffa ed Età)
+
+        plt.figure(figsize=(10,5))
+        self.df['Tariffa_Mensile'].plot(kind='bar')
+        plt.title('Distribuzione delle Tariffe Mensili')
+        plt.xlabel('Indice')
+        plt.ylabel('Tariffa Mensile\n')
+        plt.show()
+
+        plt.figure(figsize=(10,5))
+        self.df['Età'].plot(kind='bar')
+        plt.title('Distribuzione delle Età')
+        plt.xlabel('Indice')
+        plt.ylabel('Età\n')
+        plt.show()
+
+        plt.figure(figsize=(10,5))  #questo mostra la relazione tra le due colonne selezionate
+        plt.scatter(self.df['Età'], self.df['Tariffa_Mensile'])
+        plt.title('Relazione tra Età e Tariffa Mensile')
+        plt.xlabel('Età')
+        plt.ylabel('Tariffa Mensile')
+        plt.show()
+
+    def salva_dataframe_aggiornato(self):   #salvataggio 
+        
+        self.df.to_csv('C:\\Users\\matte\\AcademyItConsulting\\Esercitazioni\\18.07\\dataframe_finale.csv', index=False)
+        print("DataFrame salvato")
+        return self.df
+
+
+    
+
 
    
 
